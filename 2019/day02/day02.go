c6ff45e2c2cadf4c353322eb3500fd0c59237123
kalq <bassam.saeed@gmail.com>
2019-12-02 11:29:41 -0500

    finished part 2

--- 2019/day02/day02.go
+++ 2019/day02/day02.go
@@ -11,29 +11,46 @@ func main() {
 	file, _ := ioutil.ReadFile("input.txt")
 	strIntcode := strings.Split(string(file), ",")
 
-	intcode := make([]int, len(strIntcode))
+	pint := make([]int, len(strIntcode))
 
 	for x := 0; x < len(strIntcode); x++ {
-		intcode[x], _ = strconv.Atoi(strIntcode[x])
+		pint[x], _ = strconv.Atoi(strIntcode[x])
 	}
 
-	for x := 0; x < len(intcode); x += 4 {
-		optcode := intcode[x]
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			intcode := make([]int, len(pint))
+			copy(intcode, pint)
+			
+			intcode[1] = i
+			intcode[2] = j
+			
+			for x := 0; x < len(intcode); x += 4 {
+				optcode := intcode[x]
 
-		if optcode == 99 {
-			break
-		}
+				if optcode == 99 {
+					break
+				}
 
-		first := intcode[x+1]
-		second := intcode[x+2]
-		final := intcode[x+3]
+				firstParam := intcode[x+1]
+				secondParam := intcode[x+2]
+				finalParam := intcode[x+3]
 
-		if optcode == 1 {
-			intcode[final] = intcode[first] + intcode[second]
-		} else if optcode == 2 {
-			intcode[final] = intcode[first] * intcode[second]
+				if optcode == 1 {
+					intcode[finalParam] = intcode[firstParam] + intcode[secondParam]
+				} else if optcode == 2 {
+					intcode[finalParam] = intcode[firstParam] * intcode[secondParam]
+				}
+			}
+
+			// Part 1 Answer
+			fmt.Println(intcode)
+
+			// Part 2 Answer
+			if intcode[0] == 19690720 {
+				fmt.Println(100 * i + j)
+				break
+			}
 		}
 	}
-
-	fmt.Println(intcode)
 }
