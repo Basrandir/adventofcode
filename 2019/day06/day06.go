2b6123519cfc09978099895d16726ec6d891383d
Bassam Saeed <bassam.saeed@gmail.com>
2019-12-19 03:13:41 -0500

    part 2. build path of to root, check second path with first

--- 2019/day06/day06.go
+++ 2019/day06/day06.go
@@ -16,12 +16,36 @@ func getPaths(count int, m map[string]string, val string) int {
 	}
 }
 
+func findCommonNode(n1, n2 string, m map[string]string) int {
+	firstPath := []string{}
+
+	for m[n1] != "" {
+		firstPath = append(firstPath, m[n1])
+		n1 = m[n1]
+	}
+
+	l1 := 0
+	for m[n2] != "" {
+		l2 := 0
+		for _, p := range firstPath {
+			if p == m[n2] {
+				return l1+l2
+			}
+			l2++
+		}
+		n2 = m[n2]
+		l1++
+	}
+	return 0
+}
+
 func main() {
 	file, _ := ioutil.ReadFile("input.txt")
 	input := strings.Split(string(file), "\n")
 	input = input[:len(input)-1]
 
-	/*input = []string{
+	/*
+	input = []string{
 		"COM)B",
 		"B)C",
 		"C)D",
@@ -32,7 +56,10 @@ func main() {
 		"D)I",
 		"E)J",
 		"J)K",
-		"K)L"}*/
+		"K)L",
+		"K)YOU",
+		"I)SAN"}
+*/
 
 	m := map[string]string{}
 
@@ -41,11 +68,14 @@ func main() {
 		m[orbit[1]] = orbit[0]
 	}
 
+	// Part 1
 	count := 0
 	for key, _ := range m {
 		count = getPaths(count, m, key)
 	}
-
 	fmt.Println(count)
 
+	// Part 2
+	count = findCommonNode("YOU", "SAN", m)
+	fmt.Println(count)
 }
