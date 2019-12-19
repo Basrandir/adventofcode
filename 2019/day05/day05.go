662dbaa4581bc2c716e28ee03e670a68b0806428
Bassam Saeed <bassam.saeed@gmail.com>
2019-12-19 02:31:18 -0500

    Part 2. Spent 3 days trying to find a bug. It was whitespace...

--- 2019/day05/day05.go
+++ 2019/day05/day05.go
@@ -12,7 +12,27 @@ func parseFirstInstruction(first int) (int, []int) {
 	paramModes := []int{}
 	first /= 100
 
-	for first > 0 {
+	num := 0
+	switch opcode {
+	case 1:
+		num = 3
+	case 2:
+		num = 3
+	case 3:
+		num = 1
+	case 4:
+		num = 1
+	case 5:
+		num = 2
+	case 6:
+		num = 2
+	case 7:
+		num = 3
+	case 8:
+		num = 3
+	}
+
+	for i := 0; i < num; i++ {
 		paramModes = append(paramModes, first%10)
 		first /= 10
 	}
@@ -27,18 +47,20 @@ func main() {
 	pint := make([]int, len(input))
 
 	for x := 0; x < len(pint); x++ {
-		pint[x], _ = strconv.Atoi(input[x])
+		pint[x], _ = strconv.Atoi(strings.TrimSpace(input[x]))
 	}
 
 loop:
 	for i := 0; i < len(pint); {
 		opcode, modes := parseFirstInstruction(pint[i])
-		
-		params := []int{pint[i+1], pint[i+2], pint[i+3]}
+
+		params := []int{}
 
 		for j, mode := range modes {
 			if mode == 1 {
-				params[j] = i+j+1
+				params = append(params, i+j+1)
+			} else {
+				params = append(params, pint[i+j+1])
 			}
 		}
 
@@ -50,11 +72,37 @@ loop:
 			pint[params[2]] = pint[params[0]] * pint[params[1]]
 			i += 4
 		case 3:
-			pint[params[0]] = 1
+			pint[params[0]] = 5
 			i += 2
 		case 4:
 			fmt.Println(pint[params[0]])
 			i += 2
+		case 5:
+			if pint[params[0]] != 0 {
+				i = pint[params[1]]
+			} else {
+				i += 3
+			}
+		case 6:
+			if pint[params[0]] == 0 {
+				i = pint[params[1]]
+			} else {
+				i += 3
+			}
+		case 7:
+			if pint[params[0]] < pint[params[1]] {
+				pint[params[2]] = 1
+			} else {
+				pint[params[2]] = 0
+			}
+			i += 4
+		case 8:
+			if pint[params[0]] == pint[params[1]] {
+				pint[params[2]] = 1
+			} else {
+				pint[params[2]] = 0
+			}
+			i += 4
 		case 99:
 			break loop
 		}
